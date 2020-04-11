5362bee4d41df502e938b29fa9a01cd3145187cb
scraggo <scraggo@gmail.com>
2020-04-11 15:28:28 -0500

    tweak eslint rules

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,21 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test';
 
-import cmd = require('../src')
+import cmd = require('../src');
+
+/* eslint-disable indent */
 
 describe('designpatterns1', () => {
   test
-  .stdout()
-  .do(() => cmd.run([]))
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+    .stdout()
+    .do(() => cmd.run([]))
+    .it('runs hello', ctx => {
+      expect(ctx.stdout).to.contain('hello world');
+    });
 
   test
-  .stdout()
-  .do(() => cmd.run(['--name', 'jeff']))
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
-})
+    .stdout()
+    .do(() => cmd.run(['--name', 'jeff']))
+    .it('runs hello --name jeff', ctx => {
+      expect(ctx.stdout).to.contain('hello jeff');
+    });
+});
