f1cb483a2d27bcf3473cfa7831f984946f045435
Dave Cohen <dcohen18@gmail.com>
2020-07-04 13:45:29 -0500

    Add prettier

---
+++ docs/todo.md
@@ -0,0 +1,3 @@
+# to do
+
+- [] Add eslint-config-prettier and plugins
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,20 +2,18 @@ import { expect, test } from '@oclif/test';
 
 import cmd = require('../src');
 
-/* eslint-disable indent */
-
 describe('designpatterns1', () => {
   test
     .stdout()
     .do(() => cmd.run([]))
-    .it('runs hello', ctx => {
+    .it('runs hello', (ctx) => {
       expect(ctx.stdout).to.contain('hello world');
     });
 
   test
     .stdout()
     .do(() => cmd.run(['--name', 'jeff']))
-    .it('runs hello --name jeff', ctx => {
+    .it('runs hello --name jeff', (ctx) => {
       expect(ctx.stdout).to.contain('hello jeff');
     });
 });
