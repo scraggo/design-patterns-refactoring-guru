8606edd441db1888d346253c8884efdea1c1f443
scraggo <scraggo@gmail.com>
2021-02-25 12:15:21 -0600

    Reset code for demo

--- src/creational/factory.ts
+++ src/creational/factory.ts
@@ -1,4 +1,51 @@
-import { log } from '../utils';
+/**
+ * The Creator class declares the factory method that is supposed to return an
+ * object of a Product class. The Creator's subclasses usually provide the
+ * implementation of this method.
+ */
+abstract class Creator {
+  /**
+   * Note that the Creator may also provide some default implementation of the
+   * factory method.
+   */
+  public abstract factoryMethod(): Product;
+
+  /**
+   * Also note that, despite its name, the Creator's primary responsibility is
+   * not creating products. Usually, it contains some core business logic that
+   * relies on Product objects, returned by the factory method. Subclasses can
+   * indirectly change that business logic by overriding the factory method
+   * and returning a different type of product from it.
+   */
+  public someOperation(): string {
+    // Call the factory method to create a Product object.
+    const product = this.factoryMethod();
+    // Now, use the product.
+    return `Creator: The same creator's code has just worked with ${product.operation()}`;
+  }
+}
+
+/**
+ * Concrete Creators override the factory method in order to change the
+ * resulting product's type.
+ */
+class ConcreteCreator1 extends Creator {
+  /**
+   * Note that the signature of the method still uses the abstract product
+   * type, even though the concrete product is actually returned from the
+   * method. This way the Creator can stay independent of concrete product
+   * classes.
+   */
+  public factoryMethod(): Product {
+    return new ConcreteProduct1();
+  }
+}
+
+class ConcreteCreator2 extends Creator {
+  public factoryMethod(): Product {
+    return new ConcreteProduct2();
+  }
+}
 
 /**
  * The Product interface declares the operations that all concrete products must
@@ -24,77 +71,24 @@ class ConcreteProduct2 implements Product {
 }
 
 /**
- * The Creator class declares the factory method that is supposed to return an
- * object of a Product class. The Creator's subclasses usually provide the
- * implementation of this method.
- */
-abstract class Creator {
-  /**
-   * Note that the Creator may also provide some default implementation of the
-   * factory method.
-   */
-  public abstract factoryMethod(): Product;
-
-  /*
-   * Also note that, despite its name, the Creator's primary responsibility is
-   * not creating products. Usually, it contains some core business logic that
-   * relies on Product objects, returned by the factory method. Subclasses can
-   * indirectly change that business logic by overriding the factory method
-   * and returning a different type of product from it.
-   */
-  public someOperation(): string {
-    // Call the factory method to create a Product object.
-    const product = this.factoryMethod();
-    // Now, use the product.
-    return `Creator: The same creator's code has just worked with ${product.operation()}`;
-  }
-}
-
-/**
- * Concrete Creators override the factory method in order to change the
- * resulting product's type.
- */
-class ConcreteCreator1 extends Creator {
-  /*
-   * Note that the signature of the method still uses the abstract product
-   * type, even though the concrete product is actually returned from the
-   * method. This way the Creator can stay independent of concrete product
-   * classes.
-   */
-  public factoryMethod(): Product {
-    return new ConcreteProduct1();
-  }
-}
-
-class ConcreteCreator2 extends Creator {
-  public factoryMethod(): Product {
-    return new ConcreteProduct2();
-  }
-}
-
-/*
  * The client code works with an instance of a concrete creator, albeit through
  * its base interface. As long as the client keeps working with the creator via
  * the base interface, you can pass it any creator's subclass.
  */
 function clientCode(creator: Creator) {
   // ...
-  log("Client: I'm not aware of the creator's class, but it still works.");
-  log(creator.someOperation());
+  console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
+  console.log(creator.someOperation());
   // ...
 }
 
-export function main() {
-  /*
-   * The Application picks a creator's type depending on the configuration or
-   * environment.
-   */
-  log('App: Launched with the ConcreteCreator1.');
-  clientCode(new ConcreteCreator1());
-  log('');
+/**
+ * The Application picks a creator's type depending on the configuration or
+ * environment.
+ */
+console.log('App: Launched with the ConcreteCreator1.');
+clientCode(new ConcreteCreator1());
+console.log('');
 
-  log('App: Launched with the ConcreteCreator2.');
-  clientCode(new ConcreteCreator2());
-}
-
-export const name = 'Factory';
+console.log('App: Launched with the ConcreteCreator2.');
+clientCode(new ConcreteCreator2());
---
+++ test/factory.test.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+
+// import { assignLogStubToTestContext, getArgsForCall } from '../src/utils-test';
+import { ConcreteCreator1 } from '../src/creational/factory';
+
+describe('Factory Method Pattern', function () {
+  // assignLogStubToTestContext();
+
+  // beforeEach(function () {
+  //   this.? = new ?();
+  // });
+
+  describe('main', function () {
+    it('...', function () {
+      expect('hi').to.equal('hello');
+    });
+  });
+});
